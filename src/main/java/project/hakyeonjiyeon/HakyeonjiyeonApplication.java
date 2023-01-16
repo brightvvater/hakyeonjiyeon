8502e32027543b1c7a08fa1ea0aefc7ef87470b2
hyunsoo cho <hscho199463@gmail.com>
2023-01-16 21:49:17 +0900

    Initial commit

---
+++ src/main/java/project/hakyeonjiyeon/HakyeonjiyeonApplication.java
@@ -0,0 +1,13 @@
+package project.hakyeonjiyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HakyeonjiyeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HakyeonjiyeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/hakyeonjiyeon/HakyeonjiyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package project.hakyeonjiyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakyeonjiyeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
