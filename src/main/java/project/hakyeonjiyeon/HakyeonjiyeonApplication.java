323809556c1e8b55a738cd627f4ed424cdaac436
hyunsoo cho <hscho199463@gmail.com>
2023-02-21 22:52:34 +0900

    file upload

--- src/main/java/project/hakyeonjiyeon/HakyeonjiyeonApplication.java
+++ src/main/java/project/hakyeonjiyeon/HakyeonjiyeonApplication.java
@@ -3,7 +3,13 @@ package project.hakyeonjiyeon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+		}
+)
 public class HakyeonjiyeonApplication {
 
 	public static void main(String[] args) {
